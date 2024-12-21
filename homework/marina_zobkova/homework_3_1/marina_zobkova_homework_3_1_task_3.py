0205586fc407190ded204ad319368fc0fd648f86
Rinazo <marina.zobkova87@gmail.com>
2024-12-21 22:08:38 +0000

    marina_zobkova_homework_3_1_tasks_1_2_3_4

--- homework/marina_zobkova/homework_3_1/marina_zobkova_homework_3_1_task_3.py
+++ homework/marina_zobkova/homework_3_1/marina_zobkova_homework_3_1_task_3.py
@@ -1,12 +1,9 @@
 """find the arithmetic mean and geometric mean"""
 import math
-
 a = float(input("insert a"))
 b = float(input("insert b"))
-
 arithmetic_mean = (a + b) / 2
 geometric_mean = math.sqrt(a * b)
-
 print(arithmetic_mean)
 print(geometric_mean)
 
--- homework/marina_zobkova/homework_3_1/marina_zobkova_homework_3_1_task_4.py
+++ homework/marina_zobkova/homework_3_1/marina_zobkova_homework_3_1_task_4.py
@@ -1,12 +1,8 @@
 """given the cathetuses of a right triangle. find its hypotenuse and area"""
 import math
-
 cathetus_1 = float(input("insert cathetus_1"))
 cathetus_2 = float(input("insert cathetus_2"))
-
 hypotenuse = math.sqrt(cathetus_1 ** 2 + cathetus_2 ** 2)
 area = cathetus_1 * cathetus_2 / 2
-
 print(hypotenuse)
 print(area)
-
