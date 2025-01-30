be8859e9239b2760611a0a4ecbd91dc4f5f30e3f
Rinazo <marina.zobkova87@gmail.com>
2025-01-30 15:56:55 +0000

    homework_4_done

--- homework/marina_zobkova/homework_4/marina_zobkova_homework_4.py
+++ homework/marina_zobkova/homework_4/marina_zobkova_homework_4.py
@@ -7,17 +7,17 @@ my_dict = {
     "set": {1, 2, 3, 4, 5}
 }
 # For the ‘tuple’ key
-print(my_dict["tuple"][-1])  #The last element was printed
+print(my_dict["tuple"][-1])#The last element was printed
 # For the 'list' key
-my_dict["list"].append(6)  # An element is added to the end of the list
-my_dict["list"].pop(1)  #The second element of the list was deleted
+my_dict["list"].append(6)# An element is added to the end of the list
+my_dict["list"].pop(1)#The second element of the list was deleted
 print(my_dict["list"])
 # For the ‘dict’ key
-my_dict["i am a tuple"] = (10, 20, 30, 40, 50)  # An element is added
-my_dict["dict"].pop("three")  # The element with the key "three" of the dict was deleted
+my_dict["i am a tuple"] = (10, 20, 30, 40, 50)# An element is added
+my_dict["dict"].pop("three")# The element with the key "three" of the dict was deleted
 print(my_dict["dict"])
 # For the ‘set’ key
-my_dict["set"].add(10)  # An element is added
-my_dict["set"].discard(2)  # The element "two" was deleted
+my_dict["set"].add(10)# An element is added
+my_dict["set"].discard(2)# The element "two" was deleted
 print(my_dict["set"])
 print(my_dict)
