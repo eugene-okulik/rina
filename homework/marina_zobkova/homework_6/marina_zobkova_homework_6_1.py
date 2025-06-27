e4aa5b61d6fe9a0bf26fca4252f14608d9c85299
Rinazo <marina.zobkova87@gmail.com>
2025-06-27 14:20:38 +0100

    marina_zobkova_homework_6_1

--- homework/marina_zobkova/homework_6/marina_zobkova_homework_6_1.py
+++ homework/marina_zobkova/homework_6/marina_zobkova_homework_6_1.py
@@ -1,13 +1,15 @@
 """Assignment 1"""
-import string
-
 text = (
     'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
     'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 )
-clean_text = text.translate(str.maketrans('', '', string.punctuation))
-words = clean_text.split()
-ing_words = []
+words = text.split()
+new_words = []
 for word in words:
-    ing_words.append(word + 'ing')
-print(' '.join(ing_words))
+    if word[-1] in '.,':
+        new_word = word[:-1] + 'ing' + word[-1]
+    else:
+        new_word = word + 'ing'
+    new_words.append(new_word)
+result = ' '.join(new_words)
+print(result)
